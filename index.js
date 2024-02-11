00cfe1e0a0e0d6806b40a514331787e3a9221aba
Roman Larionov <rommalarionov00@gmail.com>
2024-02-11 21:56:28 +0800

    add search for city

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const keyApi = "1f79e1693391fcda62a5ff8f2104cbef" 
-const urlApi = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=berlin&appid=${keyApi}`
+const urlApi = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=`
+const valueInput = document.querySelector('.inputSearch');
+const buttonSearch = document.querySelector('.buttonSearch');
 
-async function checkWeather () {
-    const response = await fetch(urlApi);
+
+async function checkWeather (city) {
+    const response = await fetch(urlApi + city + `&appid=${keyApi}`);
     const data = await response.json();
-    console.log (data, "data")
-    document.querySelector('.temp').innerHTML = data.main.temp;
+    console.log (data, "data");
+    document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + "&#8451";
     document.querySelector('.current_city').innerHTML = data.name;
+    document.querySelector('.wind').innerHTML = Math.round(data.wind.speed) + " км/ч";
+    document.querySelector('.vlajnost').innerHTML = data.main.humidity + "%";
 }  
+
+buttonSearch.addEventListener('click', () => {
+    checkWeather(valueInput.value);
+    valueInput.value = "";
+} )
 checkWeather();
 
-document.getElementById('buttonSearch').onclick = butttonClick;
-function butttonClick () {
-   
-    let valueInput = document.getElementById('inputSearch').value;
-    console.log(valueInput)
-}
